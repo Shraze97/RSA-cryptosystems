74f4e10e18a15f4f998beb3dc7c23cbc8118b258
Suryansh Shrivastava <suryanshs@iisc.ac.in>
2023-02-26 19:24:37 +0530

    changes

--- utils/Conditions_to_satisfy.lean
+++ utils/Conditions_to_satisfy.lean
@@ -1,3 +1,20 @@
 import Mathlib
-import RSACryptosystems
-import utils.Lemmas
+import RSAcryptosystems
+
+
+theorem mod_pow_eq :  mod_pow a b n = (a ^ b) % n :=
+  by
+  rw[mod_pow]
+  by_cases h : b = 0 
+  · simp[h]
+    
+  · simp[mod_pow_aux_eq h]
+
+
+theorem fermat_little_theorem (p : ℕ) (hp : Prime p) (a : ℕ) : a ^ (p - 1) % p = 1 :=
+  by 
+    sorry 
+
+theorem ende : (decryption e n (encryption e n m)) = m :=
+  by
+  sorry
