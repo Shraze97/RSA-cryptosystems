6ef70a7ba4e84266116f52368627c1850d1aa4c4
YouKnowWhoI-am <prayasr@iisc.ac.in>
2023-02-26 18:55:07 +0530

    Encryption and Decryption

--- utils/Lemmas.lean
+++ utils/Lemmas.lean
@@ -1,9 +1,7 @@
 import Mathlib
-import RSACryptosystems
-
 
 
 /-Fermat's Little theorem Proof -/
 theorem fermat_little_theorem (p : ℕ) (hp : Prime p) (a : ℕ) : a ^ (p - 1) % p = 1 :=
-  by 
+  by  
     sorry 
