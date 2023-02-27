fd4a47284763896222a28366b4be1b512d9abdb3
YouKnowWhoI-am <prayasr@iisc.ac.in>
2023-02-27 07:47:18 +0530

    freshman_update

--- utils/Conditions_to_satisfy.lean
+++ utils/Conditions_to_satisfy.lean
@@ -1,8 +1,6 @@
 import Mathlib
 import RSACryptosystems
 
-
-
 theorem mod_pow_eq :  mod_pow a b n = (a ^ b) % n :=
   by
   rw[mod_pow]
@@ -13,15 +11,38 @@ theorem mod_pow_eq :  mod_pow a b n = (a ^ b) % n :=
   · simp[h, h']
     sorry
 
---#check Nat.Finset.BigOperators.Commute.add_pow
-theorem freshman's_dream (a b : ℕ) (hp : Prime p) : (a + b) ^ p = a ^ p + b ^ p :=
-  by 
+#check Commute.add_pow
+#check Nat.Prime.dvd_choose_self
+
+theorem freshman's_dream (a b : ℕ) (hp : Nat.Prime p) : ((a + b) ^ p) % p = (a ^ p + b ^ p)%p := by
+  rw[← Nat.ModEq]
+  rw[add_pow]
+  rw[Nat.ModEq.comm]
+  have h1 : {0, p} ⊆  Finset.range (p + 1) := by 
+    rw[Finset.subset_iff]
+    simp 
+  rw[←Finset.sum_sdiff h1 ]
+  have h2 : 0 ≠p := by 
+    sorry
+  rw[Finset.sum_pair h2] 
+  simp 
+  rw[Nat.modEq_iff_dvd']
+  rw[Nat.sub_add_eq,← Nat.add_assoc]
+  simp
   sorry
 
+  
 
-theorem fermat_little_theorem (p : ℕ) (hp : Prime p) (a : ℕ) : a ^ (p - 1) % p = 1 :=
+  
+
+
+
+
+
+
+
+theorem fermat_little_theorem (p : ℕ) (hp : Nat.Prime p) (a : ℕ) : a ^ (p - 1) % p = 1 :=
   sorry
-
 
 theorem ende : (decryption e n (encryption e n m)) = m :=
   by
