abc5796ab58e6bea70f591273e6ddf50edea2658
YouKnowWhoI-am <prayasr@iisc.ac.in>
2023-04-18 18:13:49 +0530

    mod_pow

--- RSACryptosystems.lean
+++ RSACryptosystems.lean
@@ -2,8 +2,7 @@ import Mathlib
 import Init 
 
 
-
-def mod_pow (a : ℕ) (b : ℕ) (n : ℕ)(hneq : n ≠ 0 ) : ℕ :=
+def mod_pow (a : ℕ)(b : ℕ)(n : ℕ)(hneq : n ≠ 0) : ℕ :=
   match b with 
   | 0 => 1
   | 1 => a % n
@@ -15,16 +14,20 @@ def mod_pow (a : ℕ) (b : ℕ) (n : ℕ)(hneq : n ≠ 0 ) : ℕ :=
     (a * (mod_pow a (k + 1) n hneq)) % n
 termination_by _ _ => b
 decreasing_by      
-have h : 1 < 2 := by norm_num
 simp 
-have h3 : k/2  + 1 < k + 1 + 1 := by
-  sorry
+have h3 : k/2 + 1 < k + 1 + 1 := by
+  simp
+  have h4 : k/2 ≤ k := by
+    apply Nat.div_le_self
+  have h5 : k < k + 1 := by
+    simp
+  apply Nat.lt_of_le_of_lt h4
+  apply h5 
 have h4 : k + 1 < k + 1 + 1 := by
-  sorry
+  simp
 simp[Nat.succ_eq_add_one] 
 try apply h3
 try apply h4
-
 
 
 -- def inverse (a : ℕ) (b : ℕ)(h : (Nat.gcd a b) = 1) : ℕ :=
@@ -32,15 +35,18 @@ try apply h4
 def inverse (a : ℕ) (b : ℕ)(h : (Nat.gcd a b) = 1) : ℕ := 
   let (x, _) := Nat.xgcd a b
   if x < 0 then 
-
-  
+  sorry
+  else
+  sorry  
 
 structure Public_key  where 
   n : ℕ 
   e : ℕ 
   hneq0 : n ≠ 0 
   deriving Repr
+
 #check Public_key 
+
 structure Key_pair extends Public_key where
   p : ℕ
   hp : Nat.Prime p
@@ -50,6 +56,7 @@ structure Key_pair extends Public_key where
   hn : n = p * q
   he : 2 < e ∧ Nat.gcd e (Nat.lcm (p - 1) (q - 1)) = 1
   deriving Repr
+  
 
 /- The key generation Function-/
 def value_d(a : Key_pair) : ℕ   :=
@@ -77,5 +84,4 @@ def encryption (a : Public_key) (m : ℕ) : ℕ :=
 def decryption (b : Private_key)(me : ℕ) : ℕ := 
   mod_pow me b.d b.n b.hneq0
 
-
-
+#check Nat.lt_add_right
